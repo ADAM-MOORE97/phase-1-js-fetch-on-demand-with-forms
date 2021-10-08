6812b945432130c41adb54710e155454a634e5d8
Adam <adammoore118@outlook.com>
2021-10-08 09:58:07 -0400

    commit tree c4378558d139f16a85cda8605f6250b78a224c66

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 const init = () => {
+
+    let form = document.querySelector('form')
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault();
+        let id= document.getElementById('searchByID').value;
+        fetch(`http://localhost:3000/movies/${id}`)
+        .then(res=>res.json())
+        .then(data=>{
+            const title = document.querySelector('#movieDetails h4');
+            const summary = document.querySelector('#movieDetails p');
+            title.textContent = data.title;
+            summary.textContent = data.summary
+        })
+        form.reset()
+     
+    })
   
 }
 
